e4a243f7d1fb2b96794144f096d85660c5b7cfbf
Rasmus Ansin <rasmus.ansin@gmail.com>
2017-04-13 00:22:35 +0200

    Add Python3.6 support

--- typechecker.py
+++ typechecker.py
@@ -1,4 +1,10 @@
-from typing import UnionMeta, GenericMeta, List
+from typing import GenericMeta, List
+
+# UnionMeta was removed in 3.6
+try:
+    from typing import UnionMeta
+except ImportError:
+    from typing import Union as UnionMeta
 
 #~ out = print
 out = lambda *args, **kwargs: None
@@ -13,16 +19,20 @@ def log(t, x, depth: int):
 def typecheck(t, x, depth: int=0) -> bool:
     log(t, x, depth)
     
-    if isinstance(t, UnionMeta):
+    #~ if isinstance(t, UnionMeta):  # Python 3.5
+    if isinstance(UnionMeta, type(t)):
         indented_print("union, any of:", depth)
-        for param_t in t.__union_params__:
+        #~ for param_t in t.__union_params__:
+        for param_t in t.__args__:
             log(param_t, x, depth+1)
             if typecheck(param_t, x, depth=depth+1):
                 return True
     elif isinstance(t, GenericMeta):
         indented_print("GenericMeta", depth)
         
-        if isinstance(x, t):  # outer level check
+        #~ import pdb; pdb.set_trace()
+        #~ if isinstance(x, t):  # outer level check
+        if isinstance(x, t.__origin__):  # outer level check  # Python 3.5
             param_t = t.__args__[0]
             for param_x in x:
                 if not typecheck(param_t, param_x, depth=depth+1):
